a9ca93a06f7e0abb55d567fb15259debabd7150b
Xindi <xindizheng123@gmail.com>
2024-12-07 09:17:13 -0700

    added inventory and removed some unecessaaray files

---
+++ inventory/inventory.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+class_name Inv
+
+@export var creatures: Array[InvItem]
---
+++ inventory/inventory_item.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name InvItem
+
+@export var name : String = ""	 #item name
+@export var texture: Texture2D	 #item texure
