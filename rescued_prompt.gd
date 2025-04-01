d2d70fbff0f451a4fc38cf23edc4f3b4243b5daa
Xindi <xindizheng123@gmail.com>
2025-04-01 01:54:17 -0600

    neat version

--- rescued_prompt.gd
+++ rescued_prompt.gd
@@ -9,11 +9,13 @@ func _ready():
 	close_button.pressed.connect(_on_close_button_pressed)
 	hide()
 
+#when prompt closed, send signal to call the dialog and inventory
 func _on_close_button_pressed():
 	hide()
 	emit_signal("animal_rescued")
 	await get_tree().create_timer(1.0).timeout
 	print("Waited 1 second after rescue")
 
+#show prompt
 func show_prompt():
 	show()
