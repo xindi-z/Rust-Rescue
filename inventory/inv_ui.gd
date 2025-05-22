b0cbe3d77ac520b17b1935cca4f5e930626aca6f
Xindi <xindizheng123@gmail.com>
2025-05-22 15:54:53 -0600

    fixed invs

--- inventory/inv_ui.gd
+++ inventory/inv_ui.gd
@@ -4,19 +4,20 @@ extends Control
 #slots are all the empty slots in the grid container
 @onready var inv: Inv = preload("res://inventory/playerinv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
-
-#item pic loaded as tres in right side of panel
-@export var item: InvItem
-#hide inventory at first
-var is_open = false
+@onready var toggle_btn: Button = $"../../invButton"
+@onready var rescued_prompt = get_node("/root/Main/CanvasLayer/HBoxContainer/rescue/RescuedPrompt")
 
 #intitial state
 #updating creatures into the inventory slots
 #hide the inventory
 #if items are gathered, it will update slots
-@onready var rescued_prompt = get_node("/root/Main/CanvasLayer/HBoxContainer/rescue/RescuedPrompt")
+#item pic loaded as tres in right side of panel
+@export var item: InvItem
+#hide inventory at first
+var is_open = false
 
 func _ready():
+#	listen to inv updates
 	inv.update.connect(update_slots)
 	update_slots()
 	close()
@@ -27,6 +28,9 @@ func _ready():
 		rescued_prompt.animal_rescued.connect(on_animal_rescued)
 	else:
 		print("RescuedPrompt node not found.")
+		
+#listen to inv button
+	toggle_btn.pressed.connect(_on_ToggleButton_pressed)
 
 #adding creatures inventory(a list of the items's name and texture) into slots
 func update_slots():
@@ -34,20 +38,21 @@ func update_slots():
 		slots[i].update(inv.slots[i])
 	
 #actions could be operate during the game
-#press i to open/close inventory, should add a button to operate in phone 
+#press i to open/close inventory
 func _process(dealta):
-	if Input.is_action_just_pressed("i"): #add a button
-		if is_open:
-			close()
-		else:
-			open()
-			DialogueManager.show_dialogue_balloon(load("res://dialogs/gilamonster.dialogue"), "start")
-
+	if Input.is_action_just_pressed("toggle_inventory"):
+		_toggle_inventory()
+		
 #trigger in process to add creature into inventory
-#where this item come from? how should i set it
-#item should be stright pass to the function after sensor is pressed
-#after colleted, plot should shown(a simple story of specis background)
-
+func _on_ToggleButton_pressed():
+	_toggle_inventory()
+	
+func _toggle_inventory():
+	if is_open:
+		close()
+	else:
+		open()
+		
 func on_animal_rescued():
 	print("Rescue signal received, collecting item...")
 	collect(item)
